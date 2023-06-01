15ddd294dbcbaf011c7a7a0dae2b3f0c649ad557
Cory Anderson <cory.anderson6@yahoo.com>
2023-05-31 19:57:33 -0500

    imported route to server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const leagueRouter = require('./routes/league.router');
 require('dotenv').config();
 
 const app = express();
@@ -23,6 +24,7 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
+app.use('/api/league', leagueRouter);
 
 // Serve static files
 app.use(express.static('build'));
